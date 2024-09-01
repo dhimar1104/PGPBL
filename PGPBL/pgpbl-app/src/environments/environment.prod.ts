ccddbd4c122db81b8bb5953e9cde90ec6cd16d66
dhimarc <dhimarfadhilansyahdarojat@mail.ugm.ac.id>
2024-09-02 03:11:59 +0700

    projek baru

---
+++ PGPBL/pgpbl-app/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyCmuAL2bEGBKdCxRKrGhSHYzZuSi6D7pqA",
+    authDomain: "reciclica-52499.firebaseapp.com",
+    projectId: "reciclica-52499",
+    storageBucket: "reciclica-52499.appspot.com",
+    messagingSenderId: "446177449935",
+    appId: "1:446177449935:web:91303c62b2cfb7ebc464ba",
+    measurementId: "G-FBJMCP48VN"
+  },
+
+};
---
+++ PGPBL/pgpbl-app/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyCmuAL2bEGBKdCxRKrGhSHYzZuSi6D7pqA",
+    authDomain: "reciclica-52499.firebaseapp.com",
+    projectId: "reciclica-52499",
+    storageBucket: "reciclica-52499.appspot.com",
+    messagingSenderId: "446177449935",
+    appId: "1:446177449935:web:91303c62b2cfb7ebc464ba",
+    measurementId: "G-FBJMCP48VN"
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
